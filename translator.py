cbc4e699f11b7f0a0ee59d8d9b77523929be942e
Josiah Gubaton <josiahag7@gmail.com>
2022-06-29 16:51:24 +1000

    Add main translator code

---
+++ translator.py
@@ -0,0 +1,10 @@
+morse_code = {}
+with open("morse-code.txt") as file:
+    for line in file:
+        (key, value) = line.split()
+        morse_code[key] = value
+
+def characterToMorse(text):
+    return ' /'.join(' '.join(morse_code[char] for char in word) for word in text.split())
+def morseToCharacter(text):
+    return ' '.join(''.join(list(morse_code.keys())[list(morse_code.values()).index(char)] for char in word.split()) for word in text.split('/'))
