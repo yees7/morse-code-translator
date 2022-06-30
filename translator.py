0e41ac46e5763c87940feb2b6e86c321dee4cbca
Josiah Gubaton <josiahag7@gmail.com>
2022-06-30 11:44:27 +1000

    Use OOP in translator.py

--- translator.py
+++ translator.py
@@ -1,15 +1,18 @@
-morse_code = {}
-with open("morse-code.txt") as file:
-    for line in file:
-        (key, value) = line.split()
-        morse_code[key] = value
-def characterToMorse(text):
-    try:
-        return ' /'.join(' '.join(morse_code[char] for char in word) for word in text.split())
-    except KeyError:
-        return "KeyError: Character invalid"
-def morseToCharacter(text):
-    try:
-        return ' '.join(''.join(list(morse_code.keys())[list(morse_code.values()).index(char)] for char in word.split()) for word in text.split('/'))
-    except ValueError:
-        return "ValueError: Character invalid"
+class Translator:
+    def __init__(self):
+        self.morse_code = {}
+
+        with open("morse-code.txt") as file:
+            for line in file:
+                (key, value) = line.split()
+                self.morse_code[key] = value
+    def characterToMorse(self, text):
+        try:
+            return ' /'.join(' '.join(self.morse_code[char] for char in word) for word in text.split())
+        except KeyError:
+            return "KeyError: Character invalid"
+    def morseToCharacter(self, text):
+        try:
+            return ' '.join(''.join(list(self.morse_code.keys())[list(self.morse_code.values()).index(char)] for char in word.split()) for word in text.split('/'))
+        except ValueError:
+            return "ValueError: Character invalid"
