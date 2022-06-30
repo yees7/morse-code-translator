4df0cafe77ae90532f163981579cb9385805ef9e
Josiah Gubaton <josiahag7@gmail.com>
2022-06-30 12:32:04 +1000

    Turn text to uppercase in morse_code/translator.py

--- morse_code/translator.py
+++ morse_code/translator.py
@@ -8,11 +8,11 @@ class Translator:
                 self.morse_code[key] = value
     def characterToMorse(self, text):
         try:
-            return ' /'.join(' '.join(self.morse_code[char] for char in word) for word in text.split())
+            return ' /'.join(' '.join(self.morse_code[char] for char in word) for word in text.upper().split())
         except KeyError:
             return "KeyError: Character invalid"
     def morseToCharacter(self, text):
         try:
-            return ' '.join(''.join(list(self.morse_code.keys())[list(self.morse_code.values()).index(char)] for char in word.split()) for word in text.split('/'))
+            return ' '.join(''.join(list(self.morse_code.keys())[list(self.morse_code.values()).index(char)] for char in word.split()) for word in text.upper().split('/'))
         except ValueError:
             return "ValueError: Character invalid"
